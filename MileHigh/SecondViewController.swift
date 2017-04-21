64f51b6e65ed9cd5d2b183d9d10784967fd6eecf
jaimejgonzalez1 <jaimejgonzalez1@gmail.com>
2017-04-20 20:15:05 -0500

    first

---
+++ MileHigh/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MileHigh
+//
+//  Created by Jaime Gonzalez on 4/11/17.
+//  Copyright © 2017 columbia college. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MileHigh/coins.swift
@@ -0,0 +1,26 @@
+//
+//  COINS
+//  MileHigh
+//
+//  Created by Jaime Gonzalez on 4/11/17.
+//  Copyright © 2017 columbia college. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
